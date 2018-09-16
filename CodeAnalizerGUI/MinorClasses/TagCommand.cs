d3fc18d4a87aa1bc1073e1021ae295c42830a425
Jakub <1elstac1@gmail.com>
2018-09-16 13:47:00 +0200

    Proba zmiany MainWindow

---
+++ CodeAnalizerGUI/MinorClasses/TagCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+namespace CodeAnalizerGUI.MinorClasses
+{
+    class TagCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        Action ExecuteCommandTarget;
+        Func<bool> CanExecuteTarget;
+
+        public bool CanExecute(object parameter)
+        {
+            if (CanExecuteTarget != null)
+                return CanExecuteTarget();
+
+            if (ExecuteCommandTarget != null)
+                return true;
+
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            ExecuteCommandTarget();
+        }
+    }
+}
---
+++ CodeAnalizerGUI/Windows/MainWindow/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeAnalizerGUI.Windows;
+using System.Windows.Controls;
+using CodeAnalizerGUI.UserControls.MainWindowControls;
+namespace CodeAnalizerGUI.Windows
+{
+    class MainWindowViewModel
+    {
+        private List<UserControl> mainWindowsControls;
+        private string[] buttonsNames = new string[] { "Global statistics", "Contributors" };
+        private List<Button> mainToolBarButtons;
+        public MainWindowViewModel()
+        {
+            
+        }
+        
+        public string[] ButtonsNames { get => buttonsNames; set => buttonsNames = value; }
+
+        private void CreateButtons()
+        {
+
+        }
+
+        private void NavigatePanelButtonClick()
+        {
+            
+        }
+    }
+}
