fb6fa203c6a086d61c11001468bb9e9dd99ae33c
Jakub <1elstac1@gmail.com>
2018-09-19 11:53:36 +0200

    Dodanie LogicHoldera do przetrzymywania i zwracania interfejsow klas logiki

---
+++ CodeAnalizerGUI/Classes/LogicHolder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeAnalizer.FileAnalizerModule.Interfaces;
+using CodeAnalizer.FileAnalizerModule.Classes;
+using CodeAnalizer.GitTrackerModule.Classes;
+using CodeAnalizer;
+namespace CodeAnalizerGUI
+{
+    class LogicHolder
+    {
+        private RepoTracker repoTracker;
+        private ProjectMiner projectMiner;
+        private static LogicHolder mainHolder;
+        public static LogicHolder MainHolder{
+            get
+            {
+                if (mainHolder == null)
+                    throw new NullReferenceException("No logic has been loaded");
+                return mainHolder;
+            }
+        }
+        
+        public LogicHolder()
+        {
+            mainHolder = this;
+        }
+        public void LoadFileMiner(string[] paths)
+        {
+            projectMiner = new ProjectMiner(paths);
+        }
+        public void LoadGitTracker(string pathToRepo)
+        {
+            repoTracker = new RepoTracker(pathToRepo);            
+        }
+        public IGitChangesTracker GetGitChangesTracker()
+        {
+            return repoTracker;
+        }
+
+        public IFileMiner GetFileMiner()
+        {
+            return projectMiner;
+        }
+    }
+}
