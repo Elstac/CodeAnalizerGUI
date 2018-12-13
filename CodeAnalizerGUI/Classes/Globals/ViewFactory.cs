551c1a5fd67a1d49be76d550f3df6e5ad9c61e16
Jakub <1elstac1@gmail.com>
2018-12-13 10:18:01 +0100

    Porzadkowanie katalogow

---
+++ CodeAnalizerGUI/Classes/Globals/ViewFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CodeAnalizerGUI
+{
+    class ViewFactory
+    {
+        private static ViewFactory instance;
+        public static ViewFactory Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ViewFactory();
+
+                return instance;
+            }
+        }
+
+        private ViewFactory()
+        {
+
+        }
+
+        public UserControl CreateView(Type type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
